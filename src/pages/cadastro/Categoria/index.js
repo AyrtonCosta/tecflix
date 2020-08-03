0e1987ee55c5d4eb8176c3d7451127e2e4e8509c
ayrtooncosta <ayrtondsp30@gmail.com>
2020-08-03 12:52:39 -0300

    comitao

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,128 @@
-import React from 'react'
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
-function CadastroCategoria  (){
-    return(
-      <PageDefault>
-        <h1>
-         Cadastro de Categoria 
-        </h1>
-        
-        <Link to="/">
-          Ir para Home 
-        </Link>
-      </PageDefault>
-    )
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categorias, SetCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
   }
 
-  export default CadastroCategoria;
+  function handleChange(infosDoEvento) {
+    // const { getAttribute, value } = infosDoEvento.target;
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }
+
+  useEffect(() => {
+    console.log('blau BLAU blau ');
+    const URL_TOP = 'http://localhost:8080/categorias';
+
+    fetch(URL_TOP)
+      .then(async (respostaDoServidor) => {
+        const reposta = await respostaDoServidor.json();
+        SetCategorias([
+          ...reposta,
+        ]);
+      });
+
+  //  setTimeout(() => {
+    //     SetCategorias([
+  //      ...categorias,
+  //      {
+    //         id: 1,
+    //         nome: 'Front end ',
+    //          descricao: 'uma categoria',
+    //         cor: '#cbd1ff',
+    //       },
+    //        {
+    //         id: 2,
+    //         nome: 'back end ',
+    //         descricao: 'outra  categoria',
+    //         cor: '#cbd1ff',
+    //        },
+    //     ]);
+    //   }, 4 * 1000);
+  }, []);
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        SetCategorias([
+          ...categorias,
+          values,
+        ]);
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          value={values.descricao}
+          name="descricao"
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      {categorias.length === 0 && (
+      <div>
+        Loading ...
+      </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para Home
+      </Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
